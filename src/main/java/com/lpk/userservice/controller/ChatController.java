679e58ea46527c3146d64aa64e211fa85d2b6559
Lakshmi prasanna kumar <125391606+Chaiudbbhd@users.noreply.github.com>
2025-07-03 18:50:16 +0530

    Update ChatController.java

--- src/main/java/com/lpk/userservice/controller/ChatController.java
+++ src/main/java/com/lpk/userservice/controller/ChatController.java
@@ -1,9 +1,10 @@
 package com.lpk.userservice.controller;
 
+import java.io.IOException;
 import java.util.Map;
 
-import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lpk.userservice.service.ChatService;
-
-import io.jsonwebtoken.io.IOException;
 
 @RestController
 @RequestMapping("/api/chat")
@@ -28,7 +27,7 @@ public class ChatController {
             String reply = chatService.getChatResponse(message);
             return ResponseEntity.ok(reply);
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
         }
     }
 }
